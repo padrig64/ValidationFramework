c747588dc086e1764ae45d07d3b9196073a125eb
unknown <Padrig@Aloha.(none)>
2011-11-30 22:22:41 +0100

    Component utils

---
+++ src/main/java/validation/utils/ComponentUtils.java
@@ -0,0 +1,19 @@
+package validation.utils;
+
+import java.awt.Component;
+import java.awt.Point;
+
+public class ComponentUtils {
+
+	private ComponentUtils() {
+		// Nothing to be done
+	}
+
+	public static Point getConvenientLocation(Component master, Component slave) {
+		Point slaveLoc = new Point(0, 0);
+
+
+
+		return slaveLoc;
+	}
+}
