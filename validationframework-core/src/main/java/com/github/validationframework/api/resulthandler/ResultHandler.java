f20c243719e24dff1376628240db0194ad755c1d
padrig64 <rubbish2006@gmail.com>
2012-09-04 13:35:25 +0200

    PrintStreamResultHandler

--- validationframework-core/src/main/java/com/github/validationframework/api/resulthandler/ResultHandler.java
+++ validationframework-core/src/main/java/com/github/validationframework/api/resulthandler/ResultHandler.java
@@ -29,14 +29,14 @@ package com.github.validationframework.api.resulthandler;
  * Interface to be implemented by handlers of validation results.<br>Result handles may give feedback to the user,
  * enable/disable other components based on conditional logic, trigger other validators, etc.
  *
- * @param <R> Type of validation result.<br>It can be, for instance, an enumeration or just a boolean.
+ * @param <O> Type of validation result.<br>It can be, for instance, an enumeration or just a boolean.
  */
-public interface ResultHandler<R> {
+public interface ResultHandler<O> {
 
 	/**
 	 * Handles the specified validation result.
 	 *
 	 * @param result Validation result to be handled.
 	 */
-	public void handleResult(R result);
+	public void handleResult(O result);
 }
