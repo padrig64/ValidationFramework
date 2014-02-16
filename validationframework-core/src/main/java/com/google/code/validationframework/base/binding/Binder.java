90fd8f29324efc59b32959cc5390205b9ef20aa5
Patrick Moawad <rubbish2006@gmail.com>
2014-02-16 12:47:13 +0100

    Moved basic binding framework from experimental to base

--- validationframework-core/src/main/java/com/google/code/validationframework/base/binding/Binder.java
+++ validationframework-core/src/main/java/com/google/code/validationframework/base/binding/Binder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Patrick Moawad
+ * Copyright (c) 2014, Patrick Moawad
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -38,9 +38,12 @@ import java.util.List;
 /**
  * Utility class that can be used to help binding properties and transform their values.
  * <p/>
+ * This binder utility will create {@link Bond}s between properties. These bonds can be broken by calling their {@link
+ * Bond#dispose()} method.
+ * <p/>
  * If you are using JavaFX, you should better use JavaFX's property binding mechanism. The binding mechanism provided by
- * the ValidationFramework is mostly meant for Swing and other frameworks that can benefit from it. JavaFX has a much
- * more furnished API to achieve similar goals and much more.
+ * the ValidationFramework is very simple and mostly meant for Swing and other frameworks that can benefit from it.
+ * JavaFX has a much more furnished API to achieve similar goals and much more.
  *
  * @see ReadableProperty
  * @see WritableProperty
@@ -49,12 +52,30 @@ import java.util.List;
  */
 public final class Binder {
 
+    /**
+     * Builder class that is part of the DSL for binding properties.
+     *
+     * @param <MO> Type of data that can be read from the master property.
+     * @param <SI> Type of data that can be written to slave properties.
+     */
     public static class SingleMasterBinding<MO, SI> {
 
+        /**
+         * Master property.
+         */
         private final ReadableProperty<MO> master;
 
+        /**
+         * Master property value transformers.
+         */
         private final List<Transformer<?, ?>> transformers = new ArrayList<Transformer<?, ?>>();
 
+        /**
+         * Constructor specifying the master property to be bound and the transformers to be applied.
+         *
+         * @param master       Master property that are part of the bind.
+         * @param transformers Transformers to be applied.
+         */
         public SingleMasterBinding(ReadableProperty<MO> master, Collection<Transformer<?, ?>> transformers) {
             this.master = master;
             if (transformers != null) {
@@ -62,30 +83,79 @@ public final class Binder {
             }
         }
 
+        /**
+         * Specifies a transformer to be used to transform the master property value.
+         *
+         * @param transformer Transformer to be used by the bond.
+         * @param <TSI>       Type of output of the specified transformer.
+         *
+         * @return Builder object to continue building the bond.
+         */
         public <TSI> SingleMasterBinding<MO, TSI> transform(Transformer<SI, TSI> transformer) {
             transformers.add(transformer);
             return new SingleMasterBinding<MO, TSI>(master, transformers);
         }
 
+        /**
+         * Specifies the slave property that is part of the bind and creates the bond between the master and the slave.
+         *
+         * @param slave Slave property.
+         *
+         * @return Bond between the master and the slave.
+         */
         public SingleMasterBond<MO, SI> to(WritableProperty<SI> slave) {
             return new SingleMasterBond<MO, SI>(master, transformers, Collections.singleton(slave));
         }
 
+        /**
+         * Specifies the slave properties that are part of the bind and creates the bond between the master and the
+         * slaves.
+         *
+         * @param slaves Slave properties.
+         *
+         * @return Bond between the master and the slaves.
+         */
         public SingleMasterBond<MO, SI> to(Collection<WritableProperty<SI>> slaves) {
             return new SingleMasterBond<MO, SI>(master, transformers, slaves);
         }
 
+        /**
+         * Specifies the slave properties that are part of the bind and creates the bond between the master and the
+         * slaves.
+         *
+         * @param slaves Slave properties.
+         *
+         * @return Bond between the master and the slaves.
+         */
         public SingleMasterBond<MO, SI> to(WritableProperty<SI>... slaves) {
             return to(Arrays.asList(slaves));
         }
     }
 
+    /**
+     * Builder class that is part of the DSL for binding properties.
+     *
+     * @param <MO> Type of data that can be read from the master properties.
+     * @param <SI> Type of data that can be written to slave properties.
+     */
     public static class MultipleMasterBinding<MO, SI> {
 
+        /**
+         * Master properties.
+         */
         private final Collection<ReadableProperty<MO>> masters;
 
+        /**
+         * Master properties values transformers.
+         */
         private final List<Transformer<?, ?>> transformers = new ArrayList<Transformer<?, ?>>();
 
+        /**
+         * Constructor specifying the master properties to be bound and the transformers to be applied.
+         *
+         * @param masters      Master properties that are part of the bind.
+         * @param transformers Transformers to be applied.
+         */
         public MultipleMasterBinding(Collection<ReadableProperty<MO>> masters, Collection<Transformer<?,
                 ?>> transformers) {
             this.masters = masters;
@@ -94,19 +164,50 @@ public final class Binder {
             }
         }
 
+        /**
+         * Specifies a transformer to be used to transform the collection of master properties values.
+         *
+         * @param transformer Transformer to be used by the bond.
+         * @param <TSI>       Type of output of the specified transformer.
+         *
+         * @return Builder object to continue building the bond.
+         */
         public <TSI> MultipleMasterBinding<MO, TSI> transform(Transformer<SI, TSI> transformer) {
             transformers.add(transformer);
             return new MultipleMasterBinding<MO, TSI>(masters, transformers);
         }
 
+        /**
+         * Specifies the slave property that is part of the bind and creates the bond between the masters and the slave.
+         *
+         * @param slave Slave property.
+         *
+         * @return Bond between the masters and the slave.
+         */
         public MultipleMasterBond<MO, SI> to(WritableProperty<SI> slave) {
             return new MultipleMasterBond<MO, SI>(masters, transformers, Collections.singleton(slave));
         }
 
+        /**
+         * Specifies the slave properties that are part of the bind and creates the bond between the masters and the
+         * slaves.
+         *
+         * @param slaves Slave properties.
+         *
+         * @return Bond between the masters and the slaves.
+         */
         public MultipleMasterBond<MO, SI> to(Collection<WritableProperty<SI>> slaves) {
             return new MultipleMasterBond<MO, SI>(masters, transformers, slaves);
         }
 
+        /**
+         * Specifies the slave properties that are part of the bind and creates the bond between the masters and the
+         * slaves.
+         *
+         * @param slaves Slave properties.
+         *
+         * @return Bond between the masters and the slaves.
+         */
         public MultipleMasterBond<MO, SI> to(WritableProperty<SI>... slaves) {
             return to(Arrays.asList(slaves));
         }
