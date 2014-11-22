04b078694a4935cd3a0bb612fa30a2647b1998fe
Patrick Moawad <patmoawad@gmail.com>
2014-11-22 12:22:52 +0100

    Added ActionProperty

---
+++ validationframework-swing/src/main/java/com/google/code/validationframework/swing/property/ActionProperty.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright (c) 2014, Patrick Moawad
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.google.code.validationframework.swing.property;
+
+import com.google.code.validationframework.api.common.Disposable;
+import com.google.code.validationframework.api.transform.Transformer;
+import com.google.code.validationframework.base.property.AbstractReadableWritableProperty;
+import com.google.code.validationframework.base.transform.CastTransformer;
+
+import javax.swing.Action;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+/**
+ * Implementation of a readable/writable property representing a bean property of an {@link Action} that can be tracked
+ * using a {@link PropertyChangeListener}.
+ * <p/>
+ * It is possible to control the bean property of the action by setting the value of this property or by calling the
+ * bean property setter method of that action.
+ * <p/>
+ * Refer to {@link Action} for a list of supported property names, as well as the associated types and values.
+ *
+ * @param <P> Type of bean property.
+ *
+ * @see Action
+ */
+public class ActionProperty<P> extends AbstractReadableWritableProperty<P, P> implements Disposable {
+
+    /**
+     * Action to track the bean property of.
+     */
+    protected final Action action;
+
+    /**
+     * Name of the property to be tracked.
+     */
+    private final String propertyName;
+
+    /**
+     * Bean property tracker.
+     */
+    private final EventAdapter eventAdapter = new EventAdapter();
+
+    /**
+     * Current property value.
+     */
+    private P value = null;
+
+    private final Transformer<Object, P> castTransformer = new CastTransformer<Object, P>();
+
+    /**
+     * Flag indicating whether the {@link #setValue(Object)} call is due to a property change event.
+     */
+    private boolean updatingFromAction = false;
+
+    /**
+     * Constructor specifying the action for which the property applies.
+     *
+     * @param action       Action whose property is to be tracked.
+     * @param propertyName Name of the property to be tracked.
+     */
+    public ActionProperty(Action action, String propertyName) {
+        super();
+
+        this.action = action;
+        this.propertyName = propertyName;
+
+        // Hook to action
+        this.action.addPropertyChangeListener(eventAdapter);
+
+        // Set initial value
+        value = getPropertyValueFromAction();
+    }
+
+    /**
+     * @see Disposable#dispose()
+     */
+    @Override
+    public void dispose() {
+        // Unhook from action
+        action.removePropertyChangeListener(eventAdapter);
+    }
+
+    /**
+     * @see AbstractReadableWritableProperty#getValue()
+     */
+    @Override
+    public P getValue() {
+        return value;
+    }
+
+    /**
+     * @see AbstractReadableWritableProperty#setValue(Object)
+     */
+    @Override
+    public void setValue(P value) {
+        if (!isNotifyingListeners()) {
+            if (updatingFromAction) {
+                P oldValue = this.value;
+                this.value = value;
+                maybeNotifyListeners(oldValue, this.value);
+            } else {
+                setPropertyValueToAction(value);
+            }
+        }
+    }
+
+    protected P getPropertyValueFromAction() {
+        Object actionValue;
+
+        if ("enabled".equals(propertyName)) {
+            actionValue = action.isEnabled();
+        } else {
+            actionValue = action.getValue(propertyName);
+        }
+
+        return castTransformer.transform(actionValue);
+    }
+
+    protected void setPropertyValueToAction(P value) {
+        if ("enabled".equals(propertyName)) {
+            action.setEnabled(Boolean.TRUE.equals(value));
+        } else {
+            action.putValue(propertyName, value);
+        }
+    }
+
+    /**
+     * Bean property tracker.
+     */
+    private class EventAdapter implements PropertyChangeListener {
+
+        /**
+         * @see PropertyChangeListener#propertyChange(PropertyChangeEvent)
+         */
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            updatingFromAction = true;
+            setValue(getPropertyValueFromAction());
+            updatingFromAction = false;
+        }
+    }
+}
---
+++ validationframework-swing/src/test/java/com/google/code/validationframework/swing/property/ActionPropertyTest.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2014, Patrick Moawad
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.google.code.validationframework.swing.property;
+
+import com.google.code.validationframework.api.property.ReadableProperty;
+import com.google.code.validationframework.api.property.ReadableWritableProperty;
+import com.google.code.validationframework.api.property.ValueChangeListener;
+import org.junit.Test;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import java.awt.event.ActionEvent;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @see ActionProperty
+ */
+public class ActionPropertyTest {
+
+    private static final String DESCRIPTION1 = "First long description";
+
+    private static final String DESCRIPTION2 = "Second long description";
+
+    @Test
+    public void testInitialEnabledPropertyFromAction() {
+        Action action = new TestAction();
+        action.setEnabled(true);
+        ReadableProperty<Boolean> enabledProperty = new ActionProperty<Boolean>(action, "enabled");
+        assertTrue(enabledProperty.getValue());
+
+        action.setEnabled(false);
+        enabledProperty = new ActionProperty<Boolean>(action, "enabled");
+        assertFalse(enabledProperty.getValue());
+    }
+
+    @Test
+    public void testInitialOtherPropertyFromAction() {
+        Action action = new TestAction();
+        ReadableProperty<String> longDescriptionProperty = new ActionProperty<String>(action, Action.LONG_DESCRIPTION);
+        assertEquals(null, longDescriptionProperty.getValue());
+
+        action.putValue(Action.LONG_DESCRIPTION, DESCRIPTION1);
+        longDescriptionProperty = new ActionProperty<String>(action, Action.LONG_DESCRIPTION);
+        assertEquals(DESCRIPTION1, longDescriptionProperty.getValue());
+
+        action.putValue(Action.LONG_DESCRIPTION, DESCRIPTION2);
+        longDescriptionProperty = new ActionProperty<String>(action, Action.LONG_DESCRIPTION);
+        assertEquals(DESCRIPTION2, longDescriptionProperty.getValue());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testNonNullFromProperty() {
+        Action action = new TestAction();
+        ReadableWritableProperty<String, String> property = new ActionProperty<String>(action, Action.LONG_DESCRIPTION);
+        ValueChangeListener<String> listenerMock = (ValueChangeListener<String>) mock(ValueChangeListener.class);
+        property.addValueChangeListener(listenerMock);
+
+        assertEquals(null, property.getValue());
+        assertEquals(null, action.getValue(Action.LONG_DESCRIPTION));
+        property.setValue(DESCRIPTION1);
+        assertEquals(DESCRIPTION1, action.getValue(Action.LONG_DESCRIPTION));
+
+        property.setValue(DESCRIPTION2);
+        assertEquals(DESCRIPTION2, action.getValue(Action.LONG_DESCRIPTION));
+
+        // Check exactly two events fired
+        verify(listenerMock).valueChanged(property, null, DESCRIPTION1);
+        verify(listenerMock).valueChanged(property, DESCRIPTION1, DESCRIPTION2);
+        verify(listenerMock, times(2)).valueChanged(any(ActionProperty.class), anyString(), anyString());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testNonNullFromAction() {
+        Action action = new TestAction();
+        ReadableWritableProperty<String, String> property = new ActionProperty<String>(action, Action.LONG_DESCRIPTION);
+        ValueChangeListener<String> listenerMock = (ValueChangeListener<String>) mock(ValueChangeListener.class);
+        property.addValueChangeListener(listenerMock);
+
+        assertEquals(null, property.getValue());
+        action.putValue(Action.LONG_DESCRIPTION, DESCRIPTION1);
+        assertEquals(DESCRIPTION1, property.getValue());
+
+        action.putValue(Action.LONG_DESCRIPTION, DESCRIPTION2);
+        assertEquals(DESCRIPTION2, property.getValue());
+
+        // Check exactly two events fired
+        verify(listenerMock).valueChanged(property, null, DESCRIPTION1);
+        verify(listenerMock).valueChanged(property, DESCRIPTION1, DESCRIPTION2);
+        verify(listenerMock, times(2)).valueChanged(any(ActionProperty.class), anyString(), anyString());
+    }
+
+    /**
+     * Dummy implementation of an action for test purposes.
+     */
+    private static class TestAction extends AbstractAction {
+
+        /**
+         * Generated serial UID.
+         */
+        private static final long serialVersionUID = -7129510551813878814L;
+
+        /**
+         * @see AbstractAction#actionPerformed(ActionEvent)
+         */
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Do nothing
+        }
+    }
+}
