660ebc23e60391177fd9f4674df0e0785aafdf49
Patrick Moawad <patmoawad@gmail.com>
2014-11-30 18:01:58 +0100

    Added some experimental transformers

---
+++ validationframework-experimental/src/main/java/com/google/code/validationframework/base/transformer/collection/ToHashSetTransformer.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2014, Patrick Moawad
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.google.code.validationframework.base.transformer.collection;
+
+import com.google.code.validationframework.api.transform.Transformer;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class ToHashSetTransformer<E> implements Transformer<Collection<E>, HashSet<E>> {
+
+    /**
+     * @see Transformer#transform(Object)
+     */
+    @Override
+    public HashSet<E> transform(Collection<E> input) {
+        HashSet<E> set;
+
+        if (input == null) {
+            set = null;
+        } else if (input instanceof HashSet<?>) {
+            set = (HashSet<E>) input;
+        } else {
+            set = new HashSet<E>(input);
+        }
+
+        return set;
+    }
+}
---
+++ validationframework-experimental/src/main/java/com/google/code/validationframework/base/transformer/collection/ToListTransformer.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2014, Patrick Moawad
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.google.code.validationframework.base.transformer.collection;
+
+import com.google.code.validationframework.api.transform.Transformer;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ToListTransformer<E, I extends Collection<E>> implements Transformer<I, ArrayList<E>> {
+
+    /**
+     * @see Transformer#transform(Object)
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public ArrayList<E> transform(I input) {
+        ArrayList<E> list;
+
+        if (input == null) {
+            list = null;
+        } else if (input instanceof ArrayList<?>) {
+            list = (ArrayList<E>) input;
+        } else {
+            list = new ArrayList<E>(input);
+        }
+
+        return list;
+    }
+}
